aa215fdc7ffeba801ff5e8bfbca32e57e6b115bf
ubehruz <behruzusmonov94@gmail.com>
2020-09-27 14:15:45 +0500

    last update

--- js/app.js
+++ js/app.js
@@ -2,28 +2,11 @@ const mainCount = document.getElementById('main-count');
 const btn = document.getElementById('btn');
 
 
-if(localStorage.getItem('allCount') === null){
-    localStorage.setItem('allCount', '0');
-}
-let allCount = parseInt(localStorage.getItem('allCount'));
-let count = 0;
+const SAA = document.getElementById('SAA');
+const istigfar = document.getElementById('istigfar');
+const salavat = document.getElementById('salavat');
 
 
-btn.addEventListener('click', () => {
-    btn.style = 'font-size: 4em';
-    if(count < 33){
-    btn.innerHTML = `<span style="font-size: 22px">Subhanalloh</span><br> ${count+=1}`;   
-    }
-    if(count == 33){
-        count = 0;
-        btn.innerHTML = `<span style="font-size: 22px">Alhamdulillah</span><br> ${count+=1}`;   
-
-    }
-
-    localStorage.setItem('allCount', allCount+=1);
-    mainCount.innerText = `Jami: ${localStorage.getItem('allCount')}`;
-})
-mainCount.innerText = `Jami: ${localStorage.getItem('allCount')}`;
 
 const resetCount = document.getElementById('resetCount');
 
@@ -33,3 +16,49 @@ resetCount.addEventListener('click', ()=>{
 })
 // btn.textContent = `${count}`;
 // btn.style = 'font-size: 4em';
+
+
+SAA.addEventListener('click', () => {
+    btn.style.fontSize ='1.75em';
+    btn.innerText = `Subhanallah`;
+
+    
+if(localStorage.getItem('allCount') === null){
+    localStorage.setItem('allCount', '0');
+}
+let allCount = parseInt(localStorage.getItem('allCount'));
+let count = 0;
+
+
+btn.addEventListener('click', () => {
+    btn.style = 'font-size: 4em';
+    if(count < 34){
+    btn.innerHTML = `${count+=1}`;   
+    }
+    if(count == 34){
+        count = 0;
+        btn.innerHTML = `${count+=1}`;   
+
+    }
+
+    localStorage.setItem('allCount', allCount+=1);
+    mainCount.innerText = `Jami: ${localStorage.getItem('allCount')}`;
+})
+mainCount.innerText = `Jami: ${localStorage.getItem('allCount')}`;
+    // SAA.classList.add('active');
+});
+
+
+
+
+istigfar.addEventListener('click', () => {
+    btn.style.fontSize ='1.80em';
+    btn.innerText = `Astaghfirullah`;
+
+
+});
+
+salavat.addEventListener('click', () =>{
+    btn.style.fontSize ='1.70em';
+    btn.innerText = `Salovoti sharif`;
+});
